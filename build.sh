891a25422fa6d78885dba8e529b73d13c9578ef6
Oracle Public Cloud User <opc@loyal-fish-bastion.public.cluster.oraclevcn.com>
2020-10-26 08:15:04 +0000

    update

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-hostfile_org=/opt/hpc/hostlist.rdma
+hostfile_org=/etc/opt/oci-hpc/hostfile.rdma
 hostfile=/var/tmp/hostlist.rdma.openmpi
-mpi_setting=~/.bashrc_openmpi3
-line=`wc -l /opt/hpc/hostlist.rdma | awk '{print $1}'`
+mpi_setting=/etc/opt/oci-hpc/bashrc/.bashrc_openmpi3
+line=`wc -l ${hostfile_org} | awk '{print $1}'`
 np=`expr 36 \* ${line}` 
+
 cat ${hostfile_org} | sed 's/$/ slots=36/' > ${hostfile}
+
+sudo yum -y install gcc-c++
 
 source ${mpi_setting}
 
@@ -15,10 +18,13 @@ cd build_linux
 make
 cd bin
 
-for host in `cat /opt/hpc/hostlist.rdma`
+for host in `cat ${hostfile_org}`
 do
-    echo ${host}
+    echo -n "Running on ${host} ..."
     mpirun -np 36 ${MPI_FLAGS} -H ${host}:36 ./xhpcg
+    RESULT_FILE=`ls -1rt HPCG* | tail -1`
+    mv ${RESULT_FILE} ${host}_${RESULT_FILE}
+    echo "Done"
 done
 
-grep VALID HPCG*
+grep VALID *_HPCG*.txt
