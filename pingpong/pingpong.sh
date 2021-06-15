0edbf85743e8810c28f4b4a29b1b82fc0c09ffca
Kazuyuki Ito <31232096+kazuitox@users.noreply.github.com>
2021-06-15 17:31:54 +0900

    Update pingpong.sh

--- pingpong/pingpong.sh
+++ pingpong/pingpong.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
-source /etc/opt/oci-hpc/bashrc/.bashrc_openmpi3
-LIST=/etc/opt/oci-hpc/hostfile.rdma
+#source /etc/opt/oci-hpc/bashrc/.bashrc_openmpi3
+export PATH=/usr/mpi/gcc/openmpi-4.0.3rc4/bin:$PATH
+export LD_LIBRARY_PATH=/usr/mpi/gcc/openmpi-4.0.3rc4/lib:$LD_LIBRARY_PATH
+LIST=./hosts
 
-MAIN_NODE=`head -1 ${LIST}`
-
-for line in `cat ${LIST} | sed '1d'`
+for MAIN_NODE in `cat ${LIST}`
 do
- mpirun -H "${MAIN_NODE}:1,${line}:1" -mca btl self -x UCX_TLS=rc,self,sm -x HCOLL_ENABLE_MCAST_ALL=0 -mca coll_hcoll_enable 0 -x UCX_IB_TRAFFIC_CLASS=105 -x UCX_IB_GID_INDEX=3 --cpu-set 0 -np 2 /usr/mpi/gcc/openmpi-3.1.1rc1/tests/imb/IMB-MPI1 pingpong
- echo $line
+
+ for line in `cat ${LIST}`
+ do
+  if [ ${MAIN_NODE} = ${line} ]; then
+   echo skip
+   echo "            1          NA     NA         NA" | tee ${MAIN_NODE}-${line}.log
+  else
+   echo "${MAIN_NODE} -> $line"
+   mpirun -H "${MAIN_NODE}:1,${line}:1" -mca btl self -x UCX_TLS=rc,self,sm -x HCOLL_ENABLE_MCAST_ALL=0 -mca coll_hcoll_enable 0 -x UCX_IB_TRAFFIC_CLASS=105 -x UCX_IB_GID_INDEX=3 --cpu-set 0 -np 2 /usr/mpi/gcc/openmpi-4.0.3rc4/tests/imb/IMB-MPI1 pingpong | tee ${MAIN_NODE}-${line}.log
+  fi
+ done
+
 done
