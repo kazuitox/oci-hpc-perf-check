0bef339e037618817d1495180dd8d2e1c8c5e7fa
Kazuyuki Ito <31232096+kazuitox@users.noreply.github.com>
2021-07-27 13:15:37 +0900

    Update BM.Optimized3.36.sh

--- pingpong/BM.Optimized3.36.sh
+++ pingpong/BM.Optimized3.36.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #source /etc/opt/oci-hpc/bashrc/.bashrc_openmpi3
-export PATH=/usr/mpi/gcc/openmpi-4.1.0rc5/bin:$PATH
-export LD_LIBRARY_PATH=/usr/mpi/gcc/openmpi-4.1.0rc5/lib:$LD_LIBRARY_PATH
+OMPI_VER=4.0.3rc4
+export LD_LIBRARY_PATH=/usr/mpi/gcc/openmpi-${OMPI_VER}/lib:$LD_LIBRARY_PATH
+
+cat /etc/hosts | grep rdma  | awk '{print $2}' > hosts
 LIST=./hosts
 
 for MAIN_NODE in `cat ${LIST}`
@@ -14,7 +16,7 @@ do
    echo "            1          NA     NA         NA" | tee ${MAIN_NODE}-${line}.log
   else
    echo "${MAIN_NODE} -> $line"
-   mpirun -H "${MAIN_NODE}:1,${line}:1"  -mca pml ucx -x UCX_NET_DEVICES=mlx5_2:1 -x UCX_IB_TRAFFIC_CLASS=105 -x UCX_IB_GID_INDEX=3 -x HCOLL_ENABLE_MCAST_ALL=0 -x coll_hcoll_enable=0 --cpu-set 0 -np 2 /usr/mpi/gcc/openmpi-4.1.0rc5/tests/imb/IMB-MPI1 pingpong | tee ${MAIN_NODE}-${line}.log
+   /usr/mpi/gcc/openmpi-${OMPI_VER}/bin/mpirun -H "${MAIN_NODE}:1,${line}:1"  -mca pml ucx -x UCX_NET_DEVICES=mlx5_2:1 -x UCX_IB_TRAFFIC_CLASS=105 -x UCX_IB_GID_INDEX=3 -x HCOLL_ENABLE_MCAST_ALL=0 -x coll_hcoll_enable=0 --cpu-set 0 -np 2 /usr/mpi/gcc/openmpi-${OMPI_VER}/tests/imb/IMB-MPI1 pingpong | tee ${MAIN_NODE}-${line}.log
   fi
  done
 
